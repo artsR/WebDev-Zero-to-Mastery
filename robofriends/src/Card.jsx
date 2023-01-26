030a55404d7631fb3ee90625098ca8065afe8c0d
artur <>
2023-01-26 01:12:32 +0100

    Update: moved from MidJourney to robohash.org

--- robofriends/src/Card.jsx
+++ robofriends/src/Card.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
 const Card = ({ name, email, id }) => {
-    return (
-        <div className='tc bg-gris dib br3 pa3 ma2 grow bw2 shadow-3'>
-            <img alt='' src='https://cdn.midjourney.com/30e63dc0-1c18-41e1-b18d-ed8995575996/grid_0_640_N.webp' width={100} height={100} />
-            <div>
-                <h2>{name}</h2>
-                <p>{email}</p>
-            </div>
-        </div>
+  return (
+    <div className='tc bg-gris dib br3 pa3 ma2 grow bw2 shadow-3'>
+    // <img alt='' src='https://cdn.midjourney.com/30e63dc0-1c18-41e1-b18d-ed8995575996/grid_0_640_N.webp' width={200} height={200} />
+      <img alt='' src={`https://robohash.org/${id}?size=200x200&set=set4`} />
+      <div>
+          <h2>{name}</h2>
+          <p>{email}</p>
+      </div>
+    </div>
     );
 }
 
--- robofriends/src/CardList.jsx
+++ robofriends/src/CardList.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ midjourneyImages }) => {
-    const cardComponent = midjourneyImages.map((user, i) => {
-        return (
-            <Card
-                key={i}
-                id={midjourneyImages[i].id}
-                name={midjourneyImages[i].name}
-                email={midjourneyImages[i].email}
-            />
-        );
-    })
-
+  const cardComponent = midjourneyImages.map((user, i) => {
     return (
-        <div>
-            {cardComponent}
-        </div>
+      <Card
+        key={i}
+        id={midjourneyImages[i].id}
+        name={midjourneyImages[i].name}
+        email={midjourneyImages[i].email}
+      />
     );
+  })
+
+  return (
+    <div>
+      {cardComponent}
+    </div>
+  );
 }
 
 export default CardList;
